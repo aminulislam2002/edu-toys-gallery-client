04ce9d7dfb9f9674b76b1246e83d88dd180f38bb
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-18 12:52:47 -0700

    create sing up and sign in form

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,35 @@
 const SignIn = () => {
+  const handleSignIn = (event) => {
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(form, email, password);
+  };
+
   return (
-    <div>
-      <h1>This is SignIn Page</h1>
+    <div className="w-1/3 mx-auto py-5">
+      <form onSubmit={handleSignIn}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Email</span>
+          </label>
+          <input type="email" name="email" placeholder="Enter Your Email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name="password" placeholder="Enter Your Password" className="input input-bordered" />
+        </div>
+        <div className="flex justify-center items-center">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2 mx-auto my-3"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,50 @@
 const SignUp = () => {
+  const handleSignUp = (event) => {
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photoURL.value;
+    const password = form.password.value;
+    console.log(form, name, email, photo, password);
+  };
+
   return (
-    <div>
-      <h1>This is SignUp Page</h1>
+    <div className="w-1/3 mx-auto py-5">
+      <form onSubmit={handleSignUp}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Name</span>
+          </label>
+          <input type="text" name="name" placeholder="Enter Your Name" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Email</span>
+          </label>
+          <input type="email" name="email" placeholder="Enter Your Email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Photo URL</span>
+          </label>
+          <input type="url" name="photoURL" placeholder="Enter Your Photo url" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+
+          <input type="password" name="password" placeholder="Enter Your Password" className="input input-bordered" />
+        </div>
+        <div className="flex justify-center items-center">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2 mx-auto my-3"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
