5a35697e5c2d42a780900119bd319bada9baa1b2
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-20 13:56:51 -0700

    update my toys page

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
-import MyToyCard from "./MyToyCard";
+
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyToys = () => {
   const { user } = useContext(AuthContext);
@@ -16,12 +18,78 @@ const MyToys = () => {
       });
   }, [url]);
 
+  const handleDeleteToy = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/toy/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your toy has been deleted.", "success");
+              const remaining = myToys.filter((myToy) => myToy._id !== id);
+              setMyToys(remaining);
+            }
+          });
+      }
+    });
+  };
+
   return (
-    <div className="mx-10">
-      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {myToys.map((myToy) => (
-          <MyToyCard key={myToy._id} myToy={myToy} myToys={myToys} setMyToys={setMyToys}></MyToyCard>
-        ))}
+    <div className="mx-10 py-10">
+      <div className="overflow-x-auto">
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th>SL</th>
+              <th>Product Name</th>
+              <th>Seller Name</th>
+              <th>Seller Email</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Code</th>
+              <th>Quantity</th>
+              <th>
+                <div className="card-actions justify-center">Action</div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {myToys.map((toy, index) => (
+              <tr key={toy._id}>
+                <th>{index + 1}</th>
+                <td>{toy.name}</td>
+                <td>{toy.sellerName}</td>
+                <td>{toy.sellerEmail}</td>
+                <td>{toy.category}</td>
+                <td>$ {toy.price}</td>
+                <td>$ {toy.code}</td>
+                <td>{toy.quantity}</td>
+                <td>
+                  <div className="card-actions justify-center">
+                    <button className="btn bg-yellow-500 hover:bg-yellow-700 border-none ">
+                      <Link to={`/updateToy/${toy._id}`}>Update</Link>
+                    </button>
+                    <button onClick={() => handleDeleteToy(toy._id)} className="btn bg-red-500 hover:bg-red-700 border-none">
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
